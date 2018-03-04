5117ad757dae867ec0cc111a4a49131f2e603fed
Maria <mariagiraldo4@gmail.com>
2018-03-04 04:26:36 -0500

    Added  tab basic menu

---
+++ components/menu/Menu.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul class="menu">
+    <opt v-for="{ id, name, window, active, link } in options "
+    :key = "id"
+    :name = "`${ name }`"
+    :window = "`${ window }`"
+    :link = "`${ link }`"
+    v-if = "active">
+    </opt>
+  </ul>
+</template>
+
+<script>
+import Opt from '~/components/menu/option.vue'
+
+export default {
+  components: {
+    Opt
+  },
+  data () {
+    return {
+      options: [
+        { id: 1, name: 'About', window:'selft', active: true,  link: '/' },
+        { id: 2, name: 'Skills', window:'new', active: true, link: '.' },
+        { id: 3, name: 'Experience', window:'new', active: true, link: '.' },
+        { id: 4, name: 'Resume', window:'new', active: true, link: 'https://www.linkedin.com/in/mariadriada/?locale=en_US'},
+        { id: 5, name: 'Code', window:'new', active: true, link: 'https://github.com/mariagiraldo/maria-driada', active: true },
+        { id: 6, name: 'Contact', window:'new', active: true, active: true, link: '' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.menu {
+  width: inherit;
+  height: 100%;
+  list-style: none;
+  padding: 0 3rem 0 0;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+}
+</style>
---
+++ components/menu/Option.vue
@@ -0,0 +1,69 @@
+<template>
+    <li @click="change" class="tabs-item" >
+      {{ name }}
+    </li>
+</template>
+
+
+<script>
+export default {
+  props: ['name', 'link', 'window'],
+  methods: {
+    change: function (e) {
+      const tabs = Array.prototype.slice.apply(document.querySelectorAll('.tabs-item'))
+      const i = tabs.indexOf(e.target)
+      tabs.map(tab => tab.classList.remove('active'))
+      tabs[i].classList.add('active')
+
+      //Redirection validation
+      if (this.link === '.')
+      {
+        console.log('activate container')
+      }
+      else {
+        switch(this.window) {
+          case 'selft':
+              location.href = this.link
+              break;
+          case 'new':
+              window.open(this.link)
+              break;
+          default:
+              location.href = this.link
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.tabs-item {
+  background-color: #FAFAFA;
+  margin-left: 1px;
+  padding: 0.5em;
+  color: #757575;
+  min-width: auto;
+  width: 6em;
+  max-width: auto;
+  text-align: center;
+  font-size: 1.3em;
+  border-radius: 0.5em 0.5em 0 0;
+  cursor: pointer;
+
+  :hover {
+    color: #4A148C;
+    font-weight: 500;
+  }
+}
+
+.active {
+  color: #673AB7;
+  font-weight: 500;
+  transition:  0.3s;
+  -webkit-transition: 0.3s;
+}
+
+
+
+</style>
