a337e5f0bfa8a586b6c5fe7f25122c6528618805
Maria <mariagiraldo4@gmail.com>
2018-03-04 04:31:04 -0500

    Working to main menu  page - basic interaction

---
+++ pages/menu/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="menu-container">
+    <banner class="header2-container"">
+    </banner>
+    <div class="data-container">
+      <div class="dinamic-text">
+        dinamic text
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Banner from '~/components/Banner2.vue'
+
+export default {
+  components: {
+    Banner
+  }
+}
+
+
+</script>
+
+<style lang="scss">
+.menu-container {
+  background-color: #F50057;
+  min-height: 100vh;
+
+  .data-container {
+    min-height: 60vh;
+    height: 60vh;
+    max-height: auto;
+    background-color:  #FAFAFA;
+
+    .dinamic-text {
+      width: 90%;
+      height: inherit;
+      background-color:  transparent;
+      margin: 0 auto;
+
+    }
+  }
+}
+</style>
