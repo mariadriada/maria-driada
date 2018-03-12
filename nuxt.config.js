1b1664a979f8352984aa8fe51c255d5a1b8996a7
Maria <mariagiraldo4@gmail.com>
2018-03-12 09:23:24 -0500

    Added properties, fonts and vue-awesome  pluging configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+const nodeExternals = require('webpack-node-externals')
+
 module.exports = {
   /*
   ** Headers of the page
@@ -5,12 +7,13 @@ module.exports = {
   head: {
     title: 'maria-driada',
     meta: [
-      { charset: 'utf-8' },
+      { charset: 'utf-8', doctype: 'html' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: "Maria Giraldo's Web project", content: "Maria Giraldo's Web project based Nuxt.js project" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href:'https://fonts.googleapis.com/css?family=Orbitron|Oswald|Raleway|Roboto+Mono|Ubuntu'}
     ]
   },
   /*
@@ -33,6 +36,24 @@ module.exports = {
           exclude: /(node_modules)/
         })
       }
+    },
+
+    vendor: ['vue-awesome'],
+
+    extend (config, { isServer }) {
+      // ...
+      if (isServer) {
+        config.externals = [
+          nodeExternals({
+            // default value for `whitelist` is
+            // [/es6-promise|\.(?!(?:js|json)$).{1,5}$/i]
+            whitelist:[/es6-promise|\.(?!(?:js|json)$).{1,5}$/i, /^vue-awesome/]
+          })
+        ]
+      }
     }
-  }
+
+  },
+
+  plugins: ['~plugins/vue-awesome.js']
 }
